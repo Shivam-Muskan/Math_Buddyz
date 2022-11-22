d33c69c2a8bd4ee2d049115d95942d1a6fcf755b
Shivam Meena <meenashivam9650@gmail.com>
2022-11-22 22:18:59 +0530

    JamStack Based App Conversion

--- src/routes/matrix/+page.svelte
+++ src/routes/matrix/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
 	import Header from '$lib/Header.svelte';
-    type matrixData = {
-        name: string,
-        rows: number,
-        columns: number,
-        matrix: [],
-        identity: []
-    }
+	type matrixData = {
+		name: string;
+		rows: number;
+		columns: number;
+		matrix: [];
+		identity: [];
+	};
 	let matrices: matrixData[] = [];
 </script>
 
 <Header heading="Matrix Calculator" />
-
