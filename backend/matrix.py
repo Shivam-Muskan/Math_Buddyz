19cefa48525c007a8103b57944c45fd11d4e19fa
Shivam Meena <meenashivam9650@gmail.com>
2022-11-23 14:51:50 +0530

    help in matrix mul

--- backend/matrix.py
+++ backend/matrix.py
@@ -15,7 +15,7 @@ results_col = 0
 
 
 def matrix_multiplication(A, B, rowA, rowB, colA, colB):
-    result = [[0 for i in range(rowA)] for j in range(colB)]
+    result = [[0 for _ in range(rowA)] for _ in range(colB)]
     if colA == rowB:
         for i in range(rowA):
             for j in range(colB):
@@ -55,7 +55,24 @@ for i in range(num):
     print(matrices[i])
     print_matrix(matrices[i], rows, cols)
 
-matrix_multiplication(matrices[0], matrices[1], total_rows[0], total_rows[1], total_cols[0], total_cols[1])
 
-for n in range(2, num):
-    matrix_multiplication(results, matrices[n], results_row, total_rows[n], results_col, total_cols[n])
+# matrix_multiplication(matrices[0], matrices[1], total_rows[0], total_rows[1], total_cols[0], total_cols[1])
+
+# for n in range(2, num):
+#     matrix_multiplication(results, matrices[n], results_row, total_rows[n], results_col, total_cols[n])
+
+
+def matrix_mul(matrix1, matrix2):
+    result = [[sum(a * b for a, b in zip(X_row, Y_col)) for Y_col in zip(*matrix2)] for X_row in matrix1]
+    print("Matrices Before", matrices)
+    matrices[len(matrices)] = result
+
+    print("Matrices After", matrices)
+
+
+for n in range(0, len(matrices), 2):
+    print("Iteration", n)
+    matrix_mul(matrices[n], matrices[n+1])
+
+resultant_matrix = matrices[len(matrices) - 1]
+print("Resultant Matrix", print_matrix(resultant_matrix, len(resultant_matrix[0]), len(resultant_matrix)))
