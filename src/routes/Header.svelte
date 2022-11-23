d1c1f1e6a411c3214500a05866f54bc60bb783c1
Shivam Meena <meenashivam9650@gmail.com>
2022-11-23 18:13:44 +0530

    Vercel File Not Found

---
+++ src/routes/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let heading;
+</script>
+
+<section class="py-12 sm:pb-16 lg:pb-20 xl:pb-24">
+	<div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+		<div class="grid items-center max-w-5xl mx-auto gap-y-8">
+			<div class="max-w-md mx-auto text-center lg:max-w-none">
+				<h1 class="text-4xl font-normal text-white uppercase sm:text-5xl lg:text-6xl xl:text-8xl">
+					<span
+						class="text-transparent font-bold animate-text bg-clip-text bg-gradient-to-r from-[#D8B61C] via-[#A0CF20] to-[#2A9433]"
+						>{heading}</span
+					>
+				</h1>
+			</div>
+		</div>
+	</div>
+</section>
